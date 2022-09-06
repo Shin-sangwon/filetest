0ca7577c67120c29b7f8e5afc4b078f07fc49e36
Shin-sangwon <dnjsml10@gmail.com>
2022-09-06 13:29:51 +0900

    csrf 체크 끄기

--- src/main/java/com/ll/exam/app10/app/fileUpload/controller/FileUploadController.java
+++ src/main/java/com/ll/exam/app10/app/fileUpload/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app10.app.fileUpload.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,18 +14,19 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/upload")
 public class FileUploadController {
+    @Value("${custom.genFileDirPath}")
+    private String genFileDirPath;
 
     @RequestMapping("")
     @ResponseBody
-    public String upload(@RequestParam("img1") MultipartFile img1) {
-        File file = new File("c:/temp/app10/1.png");
-
+    public String upload(@RequestParam("img1") MultipartFile img1, @RequestParam("img2") MultipartFile img2) {
         try {
-            img1.transferTo(file);
-        } catch(IOException e) {
+            img1.transferTo(new File(genFileDirPath + "/1.png"));
+            img2.transferTo(new File(genFileDirPath + "/2.png"));
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return "업로드 완료";
+        return "업로드 완료!";
     }
 }
